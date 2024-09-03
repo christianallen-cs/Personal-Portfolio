6fe83c198292e677b336fc7a3aca6ee856edbd2d
christianallen-cs <christianallen213@gmail.com>
2024-09-02 22:58:21 -0400

    Removed header for navbar, imported app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Header from "./components/header/header";
+import Navbar from "./components/navbar/navbar";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+        <Navbar />
     </div>
   );
 }
