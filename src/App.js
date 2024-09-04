b7290638a11b98f3601c6f9a6bb49475038591a3
christianallen-cs <christianallen213@gmail.com>
2024-09-03 21:54:50 -0400

    Added navbar to app.js instead of separate file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react'
-import Navbar from "./components/navbar/navbar";
+import React, {useRef} from 'react'
+import ScrollToTop from "./components/ScrollToTop";
 import "./App.css"
 
 function App() {
+    const skills = useRef(null);
+    const projects = useRef(null);
+    const about = useRef(null);
+    const contact = useRef(null);
+
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: "smooth",
+        });
+    };
+
   return (
     <div className="App">
-        <Navbar />
+        <ScrollToTop />
+        <div className="main">
+            <nav className="navbar">
+                <div className="navbar-center">
+                    <ul className = "nav-links">
+                        <li onClick={() => scrollToSection(about)}>
+                            About Me
+                        </li>
+                        <li onClick={() => scrollToSection(projects)}>
+                            My Projects
+                        </li>
+                        <li onClick={() => scrollToSection(skills)}>
+                            My Skills
+                        </li>
+                        <li onClick={() => scrollToSection(contact)}>
+                            Contact Me
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div ref={about} className="about">
+            <h3>About Me</h3>
+        </div>
+        <div ref={projects} className="projects">
+            <h3>My Projects</h3>
+        </div>
+        <div ref={skills} className="skills">
+            <h3>My Skills</h3>
+        </div>
+        <div ref={contact} className="contact">
+            <h3>Contact Me</h3>
+        </div>
     </div>
   );
 }
