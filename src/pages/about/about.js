674f269afdff14203d43bab165d95bc30548b64f
christianallen-cs <christianallen213@gmail.com>
2024-09-14 20:42:51 -0400

    Changed wording

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -8,21 +8,20 @@ const About = () => {
                 my Story
             </h1>
             <h2>
-                First, thank you for taking the time to check out my website. i did this little project with react,
+                Thank you for taking the time to check out my website. i created this personal website with react,
                 javascript, and good old css. i am always looking for constructive feedback, so if you notice anything
-                that could be improved or if there is a bug, let me know!
+                that could be improved or if the viewing experience is not optimal on your device, let me know!
             </h2>
             <h2>
-                i was introduced to computer gaming from an early age, which sparked it all.
-                As i grew up playing video games, i started to tinker with the various components of my pc to
-                gain more fps and maximize my computer's performance. i learned about overclocking my gpu,
-                undervolting my cpu and even messing with the windows registry to remove unnecessary bloatware. one
-                thing led to another, and eventually, i graduated high school with no idea what i wanted to do
-                in life.. but what 18-year-old does yano?
+                i was introduced to computers from an early age, specifically computer gaming. As i grew up playing
+                video games, i started to tinker with various components of my pc to gain more fps and maximize
+                my computer's performance. i learned about overclocking my gpu, undervolting my cpu and even messing
+                with the windows registry to remove unnecessary bloatware. one thing led to another, and eventually, i
+                graduated high school with no idea what i wanted to do in life.. but what 18-year-old does yano?
             </h2>
             <h2>
                 i then went to my local community college and worked on my associate's degree while taking various
-                electives in an attempt to dial in on what i truly wanted. i landed on computer science and from there,
+                electives in an attempt to dial in on what i truly wanted. i found computer science and from there,
                 the rest is history. i have now received my bachelor's degree and am looking to find my first
                 professional software engineering role where i can make an impact by pursuing my passion.
             </h2>
